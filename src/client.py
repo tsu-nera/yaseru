b0089b861f4bc9f68d29f8870766d3d0d6c928b9
tsu-nera <fox10225fox@gmail.com>
2020-01-16 20:20:53 +0900

    update

--- src/client.py
+++ src/client.py
@@ -4,7 +4,8 @@ import pandas as pd
 import src.lib.weight as weight
 import src.lib.calory as calory
 
-DAILY_RAWDATA_PATH = "rawdata/daily.csv"
+DAILY_RAWDATA_WEIGHT_PATH = "rawdata/daily_weight.csv"
+DAILY_RAWDATA_CALORY_PATH = "rawdata/daily_calory.csv"
 
 
 def get_daily_data(year, month, day):
@@ -17,5 +18,5 @@ def get_daily_data(year, month, day):
     df_weight = pd.DataFrame(weight.get_weights(target_date, target_date))
     df_calory = pd.DataFrame(calory.get_calories(target_date, target_date))
 
-    df = pd.merge(df_weight, df_calory, how='outer', on="date")
-    df.to_csv(DAILY_RAWDATA_PATH, index=False)
+    df_weight.to_csv(DAILY_RAWDATA_WEIGHT_PATH, index=False)
+    df_calory.to_csv(DAILY_RAWDATA_CALORY_PATH, index=False)
--- tasks.py
+++ tasks.py
@@ -4,6 +4,8 @@ import src.client as client
 import src.lib.weight as weight
 import src.lib.calory as calory
 
+import pandas as pd
+
 
 @invoke.task
 def get_weights(c, base_date, end_date):
@@ -18,3 +20,25 @@ def get_calories(c, base_date, end_date):
 @invoke.task
 def get_daily(c, year, month, day):
     client.get_daily_data(year, month, day)
+
+
+@invoke.task
+def merge_daily_data(c):
+    ALL_CALORIES_PATH = "data/all_calories.csv"
+    ALL_WEIGHTS_PATH = "data/all_weights.csv"
+    DAILY_RAWDATA_WEIGHT_PATH = "rawdata/daily_weight.csv"
+    DAILY_RAWDATA_CALORY_PATH = "rawdata/daily_calory.csv"
+
+    df_all_calories = pd.read_csv(ALL_CALORIES_PATH)
+    df_all_weights = pd.read_csv(ALL_WEIGHTS_PATH)
+    df_daily_calory = pd.read_csv(DAILY_RAWDATA_CALORY_PATH)
+    df_daily_weight = pd.read_csv(DAILY_RAWDATA_WEIGHT_PATH)
+
+    def merge_to_master(df_master, df_daily):
+        return pd.concat([df_master,
+                          df_daily]).drop_duplicates().sort_values("date")
+
+    df_all_weights = merge_to_master(df_all_weights, df_daily_weight)
+    df_all_weights.to_csv(ALL_WEIGHTS_PATH, index=False)
+    df_all_calories = merge_to_master(df_all_calories, df_daily_calory)
+    df_all_calories.to_csv(ALL_CALORIES_PATH, index=False)
