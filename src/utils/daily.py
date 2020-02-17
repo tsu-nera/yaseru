26947bad7e70b26cc8c92fb9757555217640bcc9
tsu-nera <fox10225fox@gmail.com>
2020-02-17 19:43:55 +0900

    wip

--- src/utils/daily.py
+++ src/utils/daily.py
@@ -6,6 +6,11 @@ from src.lib.calory import Calory
 
 DAILY_RAWDATA_WEIGHT_PATH = "rawdata/daily_weight.csv"
 DAILY_RAWDATA_CALORY_PATH = "rawdata/daily_calory.csv"
+DAILY_RAWDATA_HEALTHPLANET_PATH = "rawdata/daily_healthplanet.csv"
+
+ALL_CALORIES_PATH = "data/all_calories.csv"
+ALL_WEIGHTS_PATH = "data/all_weights.csv"
+ALL_HEALTHPLANETS_PATH = "data/all_healthplanets.csv"
 
 
 def get_daily(year=None, month=None, day=None):
@@ -23,21 +28,24 @@ def get_daily(year=None, month=None, day=None):
 
 
 def merge_daily():
-    ALL_CALORIES_PATH = "data/all_calories.csv"
-    ALL_WEIGHTS_PATH = "data/all_weights.csv"
-    DAILY_RAWDATA_WEIGHT_PATH = "rawdata/daily_weight.csv"
-    DAILY_RAWDATA_CALORY_PATH = "rawdata/daily_calory.csv"
-
     df_all_calories = pd.read_csv(ALL_CALORIES_PATH)
     df_all_weights = pd.read_csv(ALL_WEIGHTS_PATH)
+    #     df_all_hps = pd.read_csv(ALL_HEALTHPLANETS_PATH)
+
     df_daily_calory = pd.read_csv(DAILY_RAWDATA_CALORY_PATH)
     df_daily_weight = pd.read_csv(DAILY_RAWDATA_WEIGHT_PATH)
 
-    def merge_to_master(df_master, df_daily):
+    #    df_daily_hp = pd.read_csv(DAILY_RAWDATA_HEALTHPLANET_PATH)
+
+    def _merge_to_master(df_master, df_daily):
         return pd.concat([df_master,
                           df_daily]).drop_duplicates().sort_values("date")
 
-    df_all_weights = merge_to_master(df_all_weights, df_daily_weight)
+    df_all_weights = _merge_to_master(df_all_weights, df_daily_weight)
     df_all_weights.to_csv(ALL_WEIGHTS_PATH, index=False)
-    df_all_calories = merge_to_master(df_all_calories, df_daily_calory)
+
+    df_all_calories = _merge_to_master(df_all_calories, df_daily_calory)
     df_all_calories.to_csv(ALL_CALORIES_PATH, index=False)
+
+    # df_all_hps = _merge_to_master(df_all_hps, df_daily_hp)
+    # df_all_hps.to_csv(ALL_HEALTHPLANETS_PATH, index=False)
